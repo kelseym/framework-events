26db29beac2ebcb5e8d7619fba37ae5a55584031
Rick Herrick <rherrick@gmail.com>
2011-08-29 17:44:28 -0500

    Updates to support changes in notify API.

---
+++ src/main/java/org/nrg/framework/orm/hibernate/BaseHibernateDAO.java
@@ -0,0 +1,47 @@
+/**
+ * BaseHibernateDAO
+ * (C) 2011 Washington University School of Medicine
+ * All Rights Reserved
+ *
+ * Released under the Simplified BSD License
+ *
+ * Created on Aug 29, 2011 by Rick Herrick <rick.herrick@wustl.edu>
+ */
+package org.nrg.framework.orm.hibernate;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+
+/**
+ * @param <E>
+ * @author Rick Herrick <rick.herrick@wustl.edu>
+ */
+public interface BaseHibernateDAO<E extends BaseHibernateEntity> {
+
+    public abstract void setSessionFactory(SessionFactory factory);
+
+    public abstract Serializable create(E entity);
+
+    public abstract E retrieve(long id);
+
+    public abstract void update(E entity);
+
+    public abstract void delete(E entity);
+
+    public abstract List<E> findAll();
+
+    public abstract List<E> findAllEnabled();
+
+    public abstract E findById(long id);
+
+    public abstract E findById(long id, boolean lock);
+
+    public abstract E findEnabledById(long id);
+    
+    public abstract E findEnabledById(long id, boolean lock);
+    
+    public abstract List<E> findByExample(E exampleInstance, String[] excludeProperty);
+
+}
