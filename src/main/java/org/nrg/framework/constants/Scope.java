ec140213a524ce1cd5907d93a0dfc5a60a3562a6
Rick Herrick <rick.herrick@wustl.edu>
2014-09-26 22:38:48 -0500

    Added support for extra scope, encoding and decoding.

--- src/main/java/org/nrg/framework/constants/Scope.java
+++ src/main/java/org/nrg/framework/constants/Scope.java
@@ -1,30 +1,23 @@
 package org.nrg.framework.constants;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public enum Scope {
     Site("site"),
-    Project("prj", Site),
-    Subject("subj", Project),
-    User("user", Site);
+    Project("prj"),
+    Subject("subj"),
+    User("user"),
+    DataType("datatype");
 
     Scope(final String code) {
         _code = code;
-        _failoverTo = null;
-    }
-    Scope(final String code, Scope failoverTo) {
-        _code = code;
-        _failoverTo = failoverTo;
     }
 
     public String code() {
         return _code;
-    }
-
-    public Scope failoverTo() {
-        return _failoverTo;
     }
 
     public static Scope getScope(final String code) {
@@ -47,5 +40,20 @@ public enum Scope {
 
     private static final Map<String, Scope> _scopes = new ConcurrentHashMap<String, Scope>();
     private final String _code;
-    private final Scope _failoverTo;
+
+    public static String encode(final Scope scope, final String entityId) {
+        if (scope == Site) {
+            return Site.code();
+        }
+        return String.format("%s:%s", scope.code(), entityId);
+    }
+    public static Map<String, String> decode(final String association) {
+        final String[] atoms = association.split(":", 2);
+        final Scope scope = Scope.getScope(atoms[0]);
+        final String entityId = atoms.length == 1 ? null : atoms[1];
+        Map<String, String> items = new HashMap<String, String>();
+        items.put("scope", scope.code());
+        items.put("entityId", entityId);
+        return items;
+    }
 }
