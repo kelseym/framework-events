e1624283ca9c055aebfe1cb50469806ca4dd47e4
Rick Herrick <jrherrick@wustl.edu>
2016-04-12 13:57:58 -0500

    Added XapiRestController for support of integrating REST APIs across packages for Swagger.

---
+++ src/main/java/org/nrg/framework/annotations/XapiRestController.java
@@ -0,0 +1,20 @@
+package org.nrg.framework.annotations;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.*;
+
+/**
+ * Annotation to support dynamic location of REST controllers within the XNAT XAPI framework. This not only marks the
+ * class as a standard {@link RestController Spring REST controller}, but includes the API in the XNAT Swagger API
+ * listings.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RestController
+@ResponseBody
+public @interface XapiRestController {
+    String value() default "";
+}
