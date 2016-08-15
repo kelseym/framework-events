981b3e553fb763d6b572cb4b32eec95a189186ca
Rick Herrick <jrherrick@wustl.edu>
2016-08-15 12:46:50 -0500

    XNAT-4377 XNAT-4382 Cleaned up some dependency issues that affected build of XNAT data builder plugin. Added XnatPluginApi REST API. Added SortedProperties utility class to make it a bit easier to read properties outputs.

---
+++ src/main/java/org/nrg/framework/utilities/SortedProperties.java
@@ -0,0 +1,78 @@
+package org.nrg.framework.utilities;
+
+import javax.annotation.Nonnull;
+import java.util.*;
+
+/**
+ * This class extends the standard Java Properties class to provide sorting based on the
+ */
+public class SortedProperties extends Properties {
+
+    public SortedProperties() {
+        this(true, null);
+    }
+
+    public SortedProperties(final boolean sortOnKeys) {
+        this(sortOnKeys, null);
+    }
+
+    public SortedProperties(final Properties properties) {
+        this(true, properties);
+    }
+
+    public SortedProperties(final boolean sortOnKeys, final Properties properties) {
+        super(properties);
+        _sortOnKeys = sortOnKeys;
+    }
+
+    public boolean isSortOnKeys() {
+        return _sortOnKeys;
+    }
+
+    @Override
+    public Enumeration<Object> keys() {
+        if (!_sortOnKeys) {
+            return super.keys();
+        }
+
+        final List<Object> properties  = new ArrayList<>(keySet());
+
+        Collections.sort(properties, OBJECT_COMPARATOR);
+
+        return Collections.enumeration(properties);
+    }
+
+    @Override
+    @Nonnull
+    public Collection<Object> values() {
+        if (_sortOnKeys) {
+            return super.values();
+        }
+
+        final List<Object> properties  = new ArrayList<>(super.values());
+
+        Collections.sort(properties, OBJECT_COMPARATOR);
+
+        return properties;
+    }
+
+    @Override
+    public Set<String> stringPropertyNames() {
+        if (!_sortOnKeys) {
+            return super.stringPropertyNames();
+        }
+        final List<String> propertyNames = new ArrayList<>(super.stringPropertyNames());
+        Collections.sort(propertyNames, OBJECT_COMPARATOR);
+        return new LinkedHashSet<>(propertyNames);
+    }
+
+    private static final Comparator<Object> OBJECT_COMPARATOR = new Comparator<Object>() {
+        @Override
+        public int compare(final Object first, final Object second) {
+            return first.toString().compareTo(second.toString());
+        }
+    };
+
+    private final boolean _sortOnKeys;
+}
+
---
+++ src/test/java/org/nrg/framework/utilities/SortedPropertiesTest.java
@@ -0,0 +1,73 @@
+package org.nrg.framework.utilities;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class SortedPropertiesTest {
+    private static final String[]   KEYS                = new String[] {"aa", "xx", "de", "tt", "dd", "rx"};
+    private static final String[]   VALUES              = new String[] {"1", "2", "3", "4", "5", "6"};
+    private static final String[]   SORTED_KEYS         = Arrays.copyOf(KEYS, KEYS.length);
+    private static final String[]   SORTED_VALUES       = Arrays.copyOf(VALUES, VALUES.length);
+    private static final Properties UNSORTED_PROPERTIES = new Properties();
+    static {
+        Arrays.sort(SORTED_KEYS);
+        Arrays.sort(SORTED_VALUES);
+        for (int index = 0; index < KEYS.length; index++) {
+            UNSORTED_PROPERTIES.put(KEYS[index], VALUES[index]);
+        }
+    }
+
+    @Test
+    public void testBasicSortedProperties() {
+        final SortedProperties properties = new SortedProperties();
+        properties.putAll(UNSORTED_PROPERTIES);
+
+        assertTrue(properties.isSortOnKeys());
+        assertTrue(isSortedByKey(properties));
+        assertEquals(UNSORTED_PROPERTIES.size(), properties.size());
+    }
+
+    @Test
+    public void testValueSortedProperties() {
+        final SortedProperties properties = new SortedProperties(false);
+        properties.putAll(UNSORTED_PROPERTIES);
+
+        assertFalse(properties.isSortOnKeys());
+        assertTrue(isSortedByValue(properties));
+        assertEquals(UNSORTED_PROPERTIES.size(), properties.size());
+    }
+
+    private boolean isSortedByKey(final SortedProperties properties) {
+        String previous = null;
+        for (final String current : properties.stringPropertyNames()) {
+            if (previous != null) {
+                if (current.compareTo(previous) < 0) {
+                    return false;
+                }
+            }
+            previous = current;
+        }
+        return true;
+    }
+
+    private boolean isSortedByValue(final SortedProperties properties) {
+        String previous = null;
+        for (final Object currentObject : properties.values()) {
+            assertTrue(currentObject instanceof String);
+            final String current = (String) currentObject;
+            if (previous != null) {
+                if (current.compareTo(previous) < 0) {
+                    return false;
+                }
+            }
+            previous = current;
+        }
+        return true;
+    }
+}
