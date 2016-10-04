2f6dc305757cc12ebc3bf27e1edcd4a1a899050d
Rick Herrick <jrherrick@wustl.edu>
2016-10-04 16:59:47 -0500

    Updated to 1.7.1-SNAPSHOT. Added extra functions to support externalized transactions, access to JdbcTemplate.

--- src/main/java/org/nrg/framework/orm/DatabaseHelper.java
+++ src/main/java/org/nrg/framework/orm/DatabaseHelper.java
@@ -14,7 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.Nullable;
@@ -24,6 +24,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLWarning;
 import java.util.Date;
+import java.util.concurrent.Callable;
 
 @SuppressWarnings("WeakerAccess")
 public class DatabaseHelper {
@@ -46,10 +47,22 @@ public class DatabaseHelper {
     }
 
     /**
+     * Gets the database helper's JDBC template to allow database transactions outside of the helper's standard
+     * functions.
+     *
+     * @return The internal JDBC template.
+     */
+    public JdbcTemplate getJdbcTemplate() {
+        return _template;
+    }
+
+    /**
      * Checks whether the indicated table exists in the database.
      *
      * @param table The table for which to test.
+     *
      * @return Returns true if the table exists, false otherwise.
+     *
      * @throws SQLException If an error occurs while accessing the database.
      */
     public boolean tableExists(final String table) throws SQLException {
@@ -61,12 +74,14 @@ public class DatabaseHelper {
      *
      * @param schema The schema to look in.
      * @param table  The table for which to test.
+     *
      * @return Returns true if the table exists, false otherwise.
+     *
      * @throws SQLException If an error occurs while accessing the database.
      */
     public boolean tableExists(final String schema, final String table) throws SQLException {
         try (final Connection connection = _template.getDataSource().getConnection();
-             final ResultSet results = connection.getMetaData().getTables("catalog", schema, table, new String[] {"TABLE"})) {
+             final ResultSet results = connection.getMetaData().getTables("catalog", schema, table, new String[]{"TABLE"})) {
             if (results.next()) {
                 return true;
             }
@@ -80,7 +95,9 @@ public class DatabaseHelper {
      *
      * @param table  The table for which to test.
      * @param column The column for which to test.
+     *
      * @return Returns the column type if the column exists in the table, null otherwise.
+     *
      * @throws SQLException If an error occurs while accessing the database.
      */
     @Nullable
@@ -110,11 +127,12 @@ public class DatabaseHelper {
      *
      * @param table    The table in which the column can be found.
      * @param column   The column to be altered.
-     * @param datatype The datatype to set for the column.
+     * @param dataType The datatype to set for the column.
+     *
      * @throws SQLWarning   When the specified table or column doesn't exist.
      * @throws SQLException When an error occurs executing the alter query.
      */
-    public void setColumnDatatype(final String table, final String column, final String datatype) throws SQLException {
+    public void setColumnDatatype(final String table, final String column, final String dataType) throws SQLException {
         if (!tableExists(table)) {
             throw new SQLWarning("The requested table " + table + " does not exist.");
         }
@@ -122,36 +140,105 @@ public class DatabaseHelper {
         if (type == null) {
             throw new SQLWarning("The requested column " + column + " does not exist in the table " + table + ".");
         }
-        if (!StringUtils.equals(type, datatype)) {
-            if (_transactionTemplate != null) {
-                _transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-                    @Override
-                    protected void doInTransactionWithoutResult(final TransactionStatus transactionStatus) {
-                        _setColumnDataType(table, column, datatype);
-                    }
-                });
-            } else {
-                _log.warn("No transaction template found in the application context, so I'm performing the requested operation without transactional protection.");
-                _setColumnDataType(table, column, datatype);
-            }
+        if (!StringUtils.equals(type, dataType)) {
+            executeTransaction(new SetColumnDataType(table, column, dataType), true);
         } else {
-            _log.info("Not updating datatype for column {} in the table {}, the datatype is already {}.", column, table, datatype);
+            _log.info("Not updating datatype for column {} in the table {}, the datatype is already {}.", column, table, dataType);
         }
     }
 
-    private void _setColumnDataType(final String table, final String column, final String datatype) {
-        // Add the new column with the suffix "_new" and a timestamp.
-        final String tempColumnName = column + "_new_" + Long.toString(new Date().getTime());
-        _template.execute("ALTER TABLE " + table + " ADD COLUMN " + tempColumnName + " " + datatype);
+    /**
+     * Executes the callable's <b>call()</b> method, wrapping it within a transaction if possible. Note that if a
+     * transaction manager or template wasn't available to the database helper at instantiation, this method will NOT
+     * attempt to execute the transaction! If you want your method to be executed even if no transaction support is
+     * available, call the {@link #executeTransaction(Callable, boolean)} method, setting the second parameter to
+     * <b>true</b>.
+     *
+     * @param callable A callable object that implements the desired function.
+     *
+     * @return A message with the results of the transaction. The value for this depends on your implementation.
+     */
+    public String executeTransaction(final Callable<String> callable) {
+        return executeTransaction(callable, false);
+    }
 
-        // Copy all values from the existing column into the new column
-        _template.execute("UPDATE " + table + " SET " + tempColumnName + " = " + column);
+    /**
+     * Executes the callable's <b>call()</b> method, wrapping it within a transaction if possible. Note that if a
+     * transaction manager or template wasn't available to the database helper at instantiation, this method will check
+     * the second parameter: if <b>true</b>, the code will be executed without transaction protection or rollback
+     * capabilities.
+     *
+     * @param callable                            A callable object that implements the desired function.
+     * @param executeWithoutTransactionManager    Whether the transaction should be executed if no transaction manager is available.
+     *
+     * @return A message with the results of the transaction. The value for this depends on your implementation.
+     */
+    public String executeTransaction(final Callable<String> callable, final boolean executeWithoutTransactionManager) {
+        if (_transactionTemplate != null) {
+            return _transactionTemplate.execute(new TransactionCallback<String>() {
+                @Override
+                public String doInTransaction(final TransactionStatus transactionStatus) {
+                    try {
+                        return callable.call();
+                    } catch (Exception e) {
+                        return logMessage("An error occurred executing the transaction from the callable class " + callable.getClass().getName(), e);
+                    }
+                }
+            });
+        } else if (executeWithoutTransactionManager) {
+            _log.warn("No transaction template found in the application context, so I'm performing the requested operation without transactional protection.");
+            try {
+                return callable.call();
+            } catch (Exception e) {
+                return logMessage("An error occurred executing the transaction from the callable class " + callable.getClass().getName(), e);
+            }
+        } else {
+            return logMessage("No transaction template found in the application context, will not perform the requested operation without transactional protection.");
+        }
+    }
 
-        // Drop the old column
-        _template.execute("ALTER TABLE " + table + " DROP COLUMN " + column);
+    private class SetColumnDataType implements Callable<String> {
+        public SetColumnDataType(final String table, final String column, final String dataType) {
+            _table = table;
+            _column = column;
+            _dataType = dataType;
+        }
 
-        // Move the new column to the same name as the old.
-        _template.execute("ALTER TABLE " + table + " RENAME " + tempColumnName + " TO " + column);
+        @Override
+        public String call() throws Exception {
+            // Add the new column with the suffix "_new" and a timestamp.
+            final String tempColumnName = _column + "_new_" + Long.toString(new Date().getTime());
+            _template.execute("ALTER TABLE " + _table + " ADD COLUMN " + tempColumnName + " " + _dataType);
+
+            // Copy all values from the existing column into the new column
+            _template.execute("UPDATE " + _table + " SET " + tempColumnName + " = " + _column);
+
+            // Drop the old column
+            _template.execute("ALTER TABLE " + _table + " DROP COLUMN " + _column);
+
+            // Move the new column to the same name as the old.
+            _template.execute("ALTER TABLE " + _table + " RENAME " + tempColumnName + " TO " + _column);
+            return null;
+        }
+
+        private final String _table;
+
+        private final String _column;
+        private final String _dataType;
+    }
+
+    private String logMessage(final String message) {
+        return logMessage(message, null);
+    }
+
+    private String logMessage(final String message, final Exception e) {
+        if (e != null) {
+            _log.error(message, e);
+            return message + "\n" + "Exception type: " + e.getClass().getName() + "\n" + e.getMessage();
+        } else {
+            _log.warn(message);
+            return message;
+        }
     }
 
     private static final Logger _log = LoggerFactory.getLogger(DatabaseHelper.class);
