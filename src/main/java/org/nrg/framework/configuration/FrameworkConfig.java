9035db497adc1c90fbe5dee18dbaea8350c82957
Rick Herrick <jrherrick@wustl.edu>
2017-02-13 15:19:46 -0600

    XNAT-4638 Changed objectMapperBuilder() method to reflect changes in XNAT.

--- src/main/java/org/nrg/framework/configuration/FrameworkConfig.java
+++ src/main/java/org/nrg/framework/configuration/FrameworkConfig.java
@@ -13,7 +13,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import org.nrg.framework.beans.Beans;
 import org.nrg.framework.datacache.SerializerRegistry;
+import org.nrg.framework.exceptions.NrgServiceException;
 import org.nrg.framework.orm.hibernate.HibernateEntityPackageList;
 import org.nrg.framework.services.SerializerService;
 import org.springframework.context.annotation.Bean;
@@ -21,28 +23,44 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.text.DateFormat;
+import java.util.Locale;
+import java.util.Map;
+
 @Configuration
-@ComponentScan({"org.nrg.framework.datacache.impl.hibernate","org.nrg.framework.services.impl"})
+@ComponentScan({"org.nrg.framework.datacache.impl.hibernate", "org.nrg.framework.services.impl"})
 public class FrameworkConfig {
 
     @Bean
-    public HibernateEntityPackageList frameworkEntityPackageList(){
+    public HibernateEntityPackageList frameworkEntityPackageList() {
         return new HibernateEntityPackageList("org.nrg.framework.datacache");
     }
 
     @Bean
-    public SerializerRegistry serializerRegistry(){
+    public SerializerRegistry serializerRegistry() {
         return new SerializerRegistry();
     }
 
     @Bean
-    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() throws NrgServiceException {
         return new Jackson2ObjectMapperBuilder()
                 .serializationInclusion(JsonInclude.Include.NON_NULL)
                 .failOnEmptyBeans(false)
+                .mixIns(mixIns())
+                .dateFormat(dateFormat())
                 .featuresToEnable(JsonParser.Feature.ALLOW_SINGLE_QUOTES, JsonParser.Feature.ALLOW_YAML_COMMENTS)
                 .featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS, SerializationFeature.WRITE_NULL_MAP_VALUES)
                 .modulesToInstall(new Hibernate4Module());
+    }
+
+    @Bean
+    public DateFormat dateFormat() {
+        return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM, Locale.getDefault());
+    }
+
+    @Bean
+    public Map<Class<?>, Class<?>> mixIns() throws NrgServiceException {
+        return Beans.getMixIns();
     }
 
     @Bean
