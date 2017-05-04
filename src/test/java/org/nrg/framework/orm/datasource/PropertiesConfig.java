512949ea03f45eb11dd71295e50d6eff8de3663e
Rick Herrick <jrherrick@wustl.edu>
2017-05-04 14:08:39 -0500

    Added @PropertySources() to config to get rid of warnings in Java 7 builds downstream.

--- src/test/java/org/nrg/framework/orm/datasource/PropertiesConfig.java
+++ src/test/java/org/nrg/framework/orm/datasource/PropertiesConfig.java
@@ -12,10 +12,11 @@ package org.nrg.framework.orm.datasource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource(value = "classpath:org/nrg/framework/orm/datasource/datasource.properties")
+@PropertySources(@PropertySource(value = "classpath:org/nrg/framework/orm/datasource/datasource.properties"))
 public class PropertiesConfig {
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties() {
