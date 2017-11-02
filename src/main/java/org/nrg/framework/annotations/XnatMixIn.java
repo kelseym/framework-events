0643370733bc117c27a6d7e5312e94afbd75f01f
Rick Herrick <jrherrick@wustl.edu>
2017-11-02 14:18:49 -0500

    Added support for targets attribute in XnatMixIn annotation to make it possible to apply mix-ins to multiple classes.

--- src/main/java/org/nrg/framework/annotations/XnatMixIn.java
+++ src/main/java/org/nrg/framework/annotations/XnatMixIn.java
@@ -12,19 +12,40 @@ package org.nrg.framework.annotations;
 import java.lang.annotation.*;
 
 /**
- * Associates a mix-in class with the annotated class. Mix-in classes provide a way to customize how instances of
- * associated classes are serialized by the Jackson object mapper framework. <a
- * href="http://www.baeldung.com/jackson-annotations">This article</a> describes using annotations to control JSON
- * serialization and how to use mix-in classes to apply this to other data types.
+ * Associates a mix-in class with one or more classes that require custom serialization configuration. Mix-in classes
+ * provide a way to customize how instances of associated classes are serialized by the Jackson object mapper framework.
+ * <a href="http://www.baeldung.com/jackson-annotations">This article</a> describes using annotations to control JSON
+ * serialization and how to use mix-in classes to apply this to other data types. This annotation provides different
+ * methods to configure how mix-ins are associated with the target classes:
+ *
+ * <li>
+ *     <ul>Specifying the {@link #value()} attribute associates the <i>current class</i> with the mix-in class specified
+ *     as the value. That is, <b>@XnatMixIn(Foo.class)</b> on a class <b>Bar</b> would use <b>Foo</b> as the mix-in for
+ *     <b>Bar</b>.</ul>
+ *     <ul>Specifying the {@link #targets()} attribute associates the current class <i>as a mix-in class</i> with one or
+ *     more target classes specified in the value. For example, <b>@XnatMixIn({Foo.class, Bar.class})</b> on the
+ *     <b>Zed</b> class would configure <b>Zed</b> as the mix-in class for both <b>Foo</b> and <b>Bar</b>.</ul>
+ *     <ul>Specifying both {@link #value()} and {@link #targets()} lets you associate a mix-in class with one or more
+ *     target classes without having to modify either the mix-in or the target.</ul>
+ * </li>
  */
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 @Documented
 public @interface XnatMixIn {
     /**
-     * Indicates the mix-in class to be used when serializing the annotated class.
+     * Indicates the mix-in class to be used when serializing the target class. If no {@link #targets()} are specified,
+     * the annotated class is the target.
      *
      * @return The mix-in class.
      */
-    Class<?> value();
+    Class<?>[] value() default {};
+
+    /**
+     * Indicates the class or classes that the serializer should associate with a mix-in class. If no value is set for
+     * the {@link #value()} attribute, the mix-in class is set to the annotated class.
+     *
+     * @return The target class or classes.
+     */
+    Class<?>[] targets() default {};
 }
--- src/main/java/org/nrg/framework/processors/XnatMixInAnnotationProcessor.java
+++ src/main/java/org/nrg/framework/processors/XnatMixInAnnotationProcessor.java
@@ -11,11 +11,15 @@ package org.nrg.framework.processors;
 
 import org.kohsuke.MetaInfServices;
 import org.nrg.framework.annotations.XnatMixIn;
+import org.nrg.framework.exceptions.NrgServiceError;
+import org.nrg.framework.exceptions.NrgServiceRuntimeException;
 
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.TypeElement;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,14 +28,39 @@ import java.util.Map;
 @MetaInfServices(Processor.class)
 @SupportedAnnotationTypes("org.nrg.framework.annotations.XnatMixIn")
 public class XnatMixInAnnotationProcessor extends NrgAbstractAnnotationProcessor<XnatMixIn> {
-
     /**
      * {@inheritDoc}
      */
     @Override
     protected Map<String, String> processAnnotation(final TypeElement element, final XnatMixIn mixIn) {
+        final List<String> values         = getTypeElementValues(element, "value");
+        final List<String> targets        = getTypeElementValues(element, "targets");
+        final String       annotatedClass = element.getQualifiedName().toString();
+
+        final boolean hasValue   = values != null && !values.isEmpty();
+        final boolean hasTargets = targets != null && !targets.isEmpty();
+
+        if (!hasValue && !hasTargets) {
+            throw new NrgServiceRuntimeException(NrgServiceError.ConfigurationError, "You must specify at least a mix-in class with the value attribute or .");
+        }
+
+        if (hasValue && values.size() > 1) {
+            throw new NrgServiceRuntimeException(NrgServiceError.ConfigurationError, "You can only specify a single mix-in class with the value attribute.");
+        }
+
+        // If there's a value specified for the value attribute, then that's the mix-in class. Otherwise the annotated
+        // class is the mix-in class. There's only one mix-in class that can be set for each instance of this annotation
+        // so we can set it once and not have to deal with it any more.
+        final String mixInClass = hasValue ? values.get(0) : annotatedClass;
+
+        // If there's a value specified for the targets attribute, then that specifies the classes targeted by the
+        // mix-in class. If there's not any targets specified, then the "targets" is a list of one: the annotated class.
+        final List<String> targetClasses = hasTargets ? targets : Collections.singletonList(annotatedClass);
+
         final Map<String, String> properties = new LinkedHashMap<>();
-        properties.put(element.getQualifiedName().toString(), getTypeElementValue(element, "value"));
+        for (final String targetClass : targetClasses) {
+            properties.put(targetClass, mixInClass);
+        }
         return properties;
     }
 
