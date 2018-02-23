98a195e969385efccbdba68ac26d409847068b5e
Rick Herrick <rherrick@gmail.com>
2018-02-22 18:06:13 -0600

    Added pattern to screen most XSS characters (allowed ' to remain for given and surnames).

--- src/main/java/org/nrg/framework/utilities/Patterns.java
+++ src/main/java/org/nrg/framework/utilities/Patterns.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
  * Contains re-usable regex patterns for matching strings.
  */
 public class Patterns {
-    public static final String EXPR_USERNAME = "[a-zA-Z][a-zA-Z0-9_-]{3,15}";
+    public static final String EXPR_USERNAME = "[a-zA-Z][a-zA-Z0-9_'-]{3,15}";
     public static final String EXPR_EMAIL = "[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})";
     public static final String EXPR_COMBINED = "(" + EXPR_USERNAME + ")[\\s]*<(" + EXPR_EMAIL + ")>";
 
@@ -25,4 +25,6 @@ public class Patterns {
     public static final Pattern UUID = Pattern.compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
     public static final Pattern IP_PLAIN = Pattern.compile("\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b");
     public static final Pattern IP_MASK = Pattern.compile("\\b(?:\\d{1,3}\\.){3}\\d{1,3}/\\d{1,2}\\b");
+
+    public static final Pattern LIMIT_XSS_CHARS = Pattern.compile("^[^&<>\"/]+$");
 }
