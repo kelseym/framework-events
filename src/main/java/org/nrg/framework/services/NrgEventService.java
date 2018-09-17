e42d332e7aa296d432ceb39bb591bbe7e120cc2d
Rick Herrick <jrherrick@wustl.edu>
2018-09-17 15:15:18 -0500

    Refactored event service so that all calls go down into single trigger or send internal method.

--- src/main/java/org/nrg/framework/services/NrgEventService.java
+++ src/main/java/org/nrg/framework/services/NrgEventService.java
@@ -9,173 +9,203 @@
 
 package org.nrg.framework.services;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.nrg.framework.event.EventI;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.bus.Bus;
 import reactor.bus.Event;
 import reactor.bus.EventBus;
 
+import javax.annotation.Nonnull;
+
+import static lombok.AccessLevel.PRIVATE;
+
 /**
  * The Class NrgEventService.
  */
 @Service
+@Slf4j
+@Getter(PRIVATE)
+@Accessors(prefix = "_")
 public class NrgEventService {
-	/**
-	 * Instantiates a new xft event service.
+    /**
+     * Instantiates a new xft event service.
      *
-     * @param eventBus    The event bus for the service.
-	 */
-	@Autowired
-	public NrgEventService(final EventBus eventBus) {
-		_eventBus = eventBus;
-	}
+     * @param eventBus The event bus for the service.
+     */
+    @Autowired
+    public NrgEventService(final EventBus eventBus) {
+        _eventBus = eventBus;
+    }
 
-	/** The event bus. */
-	private EventBus _eventBus;
-	
-	/**
-	 * Trigger event.
-	 *
-	 * @param eventDesc the event desc
-	 * @param event the event
-	 * @param notifyClassListeners Notify class listeners?
-	 */
-	public void triggerEvent(String eventDesc, EventI event, boolean notifyClassListeners) {
-		log.debug("Triggering event {}: {}", event, eventDesc);
-		_eventBus.notify(eventDesc, Event.wrap(event));
-		if (notifyClassListeners) {
-			_eventBus.notify(event.getClass(), Event.wrap(event));
-		}
-	}
-	
-	/**
-	 * Trigger event.
-	 *
-	 * @param eventDesc the event desc
-	 * @param event the event
-	 */
-	public void triggerEvent(String eventDesc, EventI event) {
-		triggerEvent(eventDesc,event,true);
-	}
-	
-	/**
-	 * Trigger event.
-	 *
-	 * @param event the event
-	 */
-	public void triggerEvent(EventI event) {
-		log.debug("Triggering event {}", event);
-		_eventBus.notify(event.getClass(), Event.wrap(event));
-	}
-	
-	/**
-	 * Trigger event.
-	 *
-	 * @param event the event
-	 * @param replyTo the reply to
-	 */
-	public void triggerEvent(EventI event, Object replyTo) {
-		if (replyTo == null) {
-			throw new IllegalArgumentException("Event replyTo object cannot be null");
-		}
-		log.debug("Triggering event {}", event);
-		_eventBus.notify(event.getClass(), Event.wrap(event, replyTo));
-	}
-	
-	/**
-	 * Trigger event.
-	 *
-	 * @param eventDesc the event desc
-	 * @param event the event
-	 * @param notifyClassListeners Notify class event listeners?
-	 */
-	@SuppressWarnings("rawtypes")
-	public void triggerEvent(String eventDesc,Event event,boolean notifyClassListeners) {
-		log.debug("Triggering event {}: {}", event, eventDesc);
-		_eventBus.notify(eventDesc, event);
-		if (notifyClassListeners) {
-			_eventBus.notify(event.getClass(), Event.wrap(event));
-		}
-	}
-	
-	/**
-	 * Trigger event.
-	 *
-	 * @param eventDesc the event desc
-	 * @param event the event
-	 */
-	@SuppressWarnings("rawtypes")
-	public void triggerEvent(String eventDesc,Event event) {
-		log.debug("Triggering event {}: {}", event, eventDesc);
-		triggerEvent(eventDesc,event,true);
-	}
-	
-	/**
-	 * Trigger event.
-	 *
-	 * @param event the event
-	 */
-	@SuppressWarnings("rawtypes")
-	public void triggerEvent(Event event) {
-		log.debug("Triggering event {}", event);
-		_eventBus.notify(event.getClass(), event);
-	}
-	
-	/**
-	 * Send event.
-	 *
-	 * @param event the event
-	 */
-	@SuppressWarnings("rawtypes")
-	public void sendEvent(Event event) {
-		if (event.getReplyTo() == null) {
-			throw new IllegalArgumentException("Event replyTo object cannot be null");
-		}
-		log.debug("Triggering event {}", event);
-		_eventBus.send(event.getClass(), event);
-	}
-	
-	/**
-	 * Send event.
-	 *
-	 * @param eventDesc the event desc
-	 * @param event the event
-	 * @param notifyClassListeners Notify class listeners?
-	 * @param replyTo the reply to
-	 */
-	@SuppressWarnings("rawtypes")
-	public void sendEvent(String eventDesc, EventI event, Bus replyTo, boolean notifyClassListeners) {
-		_eventBus.send(eventDesc, Event.wrap(event), replyTo);
-		if (notifyClassListeners) {
-			_eventBus.send(event.getClass(), Event.wrap(event), replyTo);
-		}
-	}
-	
-	/**
-	 * Send event.
-	 *
-	 * @param eventDesc the event desc
-	 * @param event the event
-	 * @param replyTo the reply to
-	 */
-	@SuppressWarnings("rawtypes")
-	public void sendEvent(String eventDesc, EventI event, Bus replyTo) {
-		sendEvent(eventDesc, event, replyTo, true);
-	}
-	
-	/**
-	 * Send event.
-	 *
-	 * @param event the event
-	 * @param replyTo the reply to
-	 */
-	@SuppressWarnings("rawtypes")
-	public void sendEvent(EventI event, Bus replyTo) {
-		_eventBus.send(event.getClass(), Event.wrap(event), replyTo);
-	}
+    /**
+     * Trigger event.
+     *
+     * @param description          the event desc
+     * @param event                the event
+     * @param notifyClassListeners Notify class listeners?
+     */
+    public void triggerEvent(final String description, final EventI event, final boolean notifyClassListeners) {
+        triggerEventInternal(Event.wrap(event), description, notifyClassListeners);
+    }
 
-	private final Logger log = LoggerFactory.getLogger(NrgEventService.class);
+    /**
+     * Trigger event.
+     *
+     * @param description the event desc
+     * @param event       the event
+     */
+    public void triggerEvent(final String description, final EventI event) {
+        triggerEvent(description, event, true);
+    }
+
+    /**
+     * Trigger event.
+     *
+     * @param event the event
+     */
+    public void triggerEvent(final EventI event) {
+        triggerEventInternal(Event.wrap(event), null, null);
+    }
+
+    /**
+     * Trigger event.
+     *
+     * @param event   the event
+     * @param replyTo the reply to
+     */
+    public void triggerEvent(final EventI event, final Object replyTo) {
+        if (replyTo == null) {
+            throw new IllegalArgumentException("Event replyTo object cannot be null");
+        }
+        triggerEventInternal(Event.wrap(event, replyTo), null, null);
+    }
+
+    /**
+     * Trigger event.
+     *
+     * @param description          the event desc
+     * @param event                the event
+     * @param notifyClassListeners Notify class event listeners?
+     */
+    @SuppressWarnings("rawtypes")
+    public void triggerEvent(final String description, final Event event, final boolean notifyClassListeners) {
+        triggerEventInternal(event, description, notifyClassListeners);
+    }
+
+    /**
+     * Trigger event.
+     *
+     * @param description the event desc
+     * @param event       the event
+     */
+    @SuppressWarnings("rawtypes")
+    public void triggerEvent(final String description, final Event event) {
+        triggerEventInternal(event, description, true);
+    }
+
+    /**
+     * Trigger event.
+     *
+     * @param event the event
+     */
+    @SuppressWarnings("rawtypes")
+    public void triggerEvent(final Event event) {
+        triggerEventInternal(event, null, null);
+    }
+
+    /**
+     * Send event.
+     *
+     * @param event the event
+     */
+    @SuppressWarnings({"rawtypes", "unused"})
+    public void sendEvent(final Event event) {
+        sendEventInternal(event, null, null, null, false);
+    }
+
+    /**
+     * Send event.
+     *
+     * @param description          the event desc
+     * @param event                the event
+     * @param notifyClassListeners Notify class listeners?
+     * @param replyTo              the reply to
+     */
+    @SuppressWarnings({"rawtypes", "unused"})
+    public void sendEvent(final String description, final EventI event, final Bus replyTo, final boolean notifyClassListeners) {
+        sendEventInternal(Event.wrap(event), null, description, replyTo, notifyClassListeners);
+    }
+
+    /**
+     * Send event.
+     *
+     * @param description the event desc
+     * @param event       the event
+     * @param replyTo     the reply to
+     */
+    @SuppressWarnings({"rawtypes", "unused"})
+    public void sendEvent(final String description, final EventI event, final Bus replyTo) {
+        sendEventInternal(Event.wrap(event), null, description, replyTo, true);
+    }
+
+    /**
+     * Send event.
+     *
+     * @param event   the event
+     * @param replyTo the reply to
+     */
+    @SuppressWarnings({"rawtypes", "unused"})
+    public void sendEvent(final EventI event, final Bus replyTo) {
+        sendEventInternal(Event.wrap(event), event.getClass(), null, replyTo, false);
+    }
+
+    private void triggerEventInternal(@Nonnull final Event event, final String description, final Boolean notifyClassListeners) {
+        if (StringUtils.isNotBlank(description)) {
+            log.debug("Triggering event {}: {}", event, description);
+            getEventBus().notify(description, event);
+            if (notifyClassListeners) {
+                getEventBus().notify(event.getClass(), event);
+            }
+        } else {
+            log.debug("Triggering event {}", event);
+            getEventBus().notify(event.getClass(), Event.wrap(event));
+        }
+
+        if (BooleanUtils.toBooleanDefaultIfNull(notifyClassListeners, false)) {
+            getEventBus().notify(event.getClass(), Event.wrap(event));
+        }
+    }
+
+    private void sendEventInternal(@Nonnull final Event event, final Class<? extends EventI> eventClass, final String description, final Bus replyTo, final boolean notifyClassListeners) {
+        final boolean hasReplyTo = replyTo != null;
+        if (!hasReplyTo && event.getReplyTo() == null) {
+            throw new IllegalArgumentException("Event replyTo object cannot be null");
+        }
+        final boolean  hasDescription = StringUtils.isNotBlank(description);
+        final Class<?> classToSend    = ObjectUtils.defaultIfNull(eventClass, event.getClass());
+        if (!hasDescription) {
+            log.debug("Sending event {} as class {}", event, classToSend);
+            if (hasReplyTo) {
+                getEventBus().send(classToSend, event, replyTo);
+            } else {
+                getEventBus().send(classToSend, event);
+            }
+        } else if (!hasReplyTo) {
+            getEventBus().send(description, event);
+        } else {
+            getEventBus().send(description, event, replyTo);
+        }
+        if (notifyClassListeners) {
+            getEventBus().send(classToSend, event, replyTo);
+        }
+    }
+
+    private final EventBus _eventBus;
 }
